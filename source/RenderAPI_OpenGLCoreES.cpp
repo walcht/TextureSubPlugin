2461986bbe692ed58962391748941de42cf654b4
Walid Chtioui <walid.chtioui@ensi-uma.tn>
2024-09-16 15:06:08 +0200

    format code; add SetTextureSubImage3DParams to linker module;

--- source/RenderAPI_OpenGLCoreES.cpp
+++ source/RenderAPI_OpenGLCoreES.cpp
@@ -1,140 +1,98 @@
-#include "RenderAPI.h"
 #include "PlatformBase.h"
+#include "RenderAPI.h"
 
-// OpenGL Core profile (desktop) or OpenGL ES (mobile) implementation of RenderAPI.
-// Supports several flavors: Core, ES2, ES3
-
+// OpenGL Core profile (desktop) or OpenGL ES (mobile) implementation of
+// RenderAPI. Supports several flavors: Core, ES2, ES3
 
 #if SUPPORT_OPENGL_UNIFIED
 
-
 #include <assert.h>
 #if UNITY_IOS || UNITY_TVOS
-#	include <OpenGLES/ES2/gl.h>
+#include <OpenGLES/ES2/gl.h>
 #elif UNITY_ANDROID || UNITY_WEBGL
-#	include <GLES2/gl2.h>
+#include <GLES2/gl2.h>
 #elif UNITY_OSX
-#	include <OpenGL/gl3.h>
+#include <OpenGL/gl3.h>
 #elif UNITY_WIN
-// On Windows, use gl3w to initialize and load OpenGL Core functions. In principle any other
-// library (like GLEW, GLFW etc.) can be used; here we use gl3w since it's simple and
-// straightforward.
-#	include "gl3w/gl3w.h"
+// On Windows, use gl3w to initialize and load OpenGL Core functions. In
+// principle any other library (like GLEW, GLFW etc.) can be used; here we use
+// gl3w since it's simple and straightforward.
+#include "gl3w/gl3w.h"
 #elif UNITY_LINUX
-#	define GL_GLEXT_PROTOTYPES
-#	include <GL/gl.h>
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
 #elif UNITY_EMBEDDED_LINUX
-#	include <GLES2/gl2.h>
+#include <GLES2/gl2.h>
 #if SUPPORT_OPENGL_CORE
-#	define GL_GLEXT_PROTOTYPES
-#	include <GL/gl.h>
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
 #endif
 #elif UNITY_QNX
-#	include <GLES2/gl2.h>
+#include <GLES2/gl2.h>
 #else
-#	error Unknown platform
+#error Unknown platform
 #endif
 
+class RenderAPI_OpenGLCoreES : public RenderAPI {
+ public:
+  RenderAPI_OpenGLCoreES(UnityGfxRenderer apiType);
+  virtual ~RenderAPI_OpenGLCoreES() {}
 
-class RenderAPI_OpenGLCoreES : public RenderAPI
-{
-public:
-	RenderAPI_OpenGLCoreES(UnityGfxRenderer apiType);
-	virtual ~RenderAPI_OpenGLCoreES() { }
+  virtual void ProcessDeviceEvent(UnityGfxDeviceEventType type,
+                                  IUnityInterfaces* interfaces);
+  virtual void TextureSubImage2D(void* texture_handle, int32_t xoffset,
+                                 int32_t yoffset, int32_t width, int32_t height,
+                                 void* data_ptr, int32_t level = 0,
+                                 int32_t format = -1, int32_t type = -1,
+                                 int32_t row_pitch = -1);
+  virtual void TextureSubImage3D(void* texture_handle, int32_t xoffset,
+                                 int32_t yoffset, int32_t zoffset,
+                                 int32_t width, int32_t height, int32_t depth,
+                                 void* data_ptr, int32_t level = 0,
+                                 int32_t format = -1, int32_t type = -1,
+                                 int32_t row_pitch = -1,
+                                 int32_t depth_pitch = -1);
 
-	virtual void ProcessDeviceEvent(UnityGfxDeviceEventType type, IUnityInterfaces* interfaces);
-	virtual void TextureSubImage2D(
-		void* texture_handle,
-		int32_t xoffset,
-		int32_t yoffset,
-		int32_t width,
-		int32_t height,
-		void* data_ptr,
-		int32_t level = 0,
-		int32_t format = -1,
-		int32_t type = -1,
-		int32_t row_pitch = -1
-	);
-	virtual void TextureSubImage3D(
-		void* texture_handle,
-		int32_t xoffset,
-		int32_t yoffset,
-		int32_t zoffset,
-		int32_t width,
-		int32_t height,
-		int32_t depth,
-		void* data_ptr,
-		int32_t level = 0,
-		int32_t format = -1,
-		int32_t type = -1,
-		int32_t row_pitch = -1,
-		int32_t depth_pitch = -1
-	);
-
-private:
-	UnityGfxRenderer m_APIType;
+ private:
+  UnityGfxRenderer m_APIType;
 };
 
-
-RenderAPI* CreateRenderAPI_OpenGLCoreES(UnityGfxRenderer apiType)
-{
-	return new RenderAPI_OpenGLCoreES(apiType);
+RenderAPI* CreateRenderAPI_OpenGLCoreES(UnityGfxRenderer apiType) {
+  return new RenderAPI_OpenGLCoreES(apiType);
 }
-
 
 RenderAPI_OpenGLCoreES::RenderAPI_OpenGLCoreES(UnityGfxRenderer apiType)
-	: m_APIType(apiType)
-{
-}
+    : m_APIType(apiType) {}
 
-
-void RenderAPI_OpenGLCoreES::ProcessDeviceEvent(UnityGfxDeviceEventType type, IUnityInterfaces* interfaces)
-{
-	if (type == kUnityGfxDeviceEventInitialize)
-	{
-		// nothing
-	}
-	else if (type == kUnityGfxDeviceEventShutdown)
-	{
-		//@TODO: release resources
-	}
+void RenderAPI_OpenGLCoreES::ProcessDeviceEvent(UnityGfxDeviceEventType type,
+                                                IUnityInterfaces* interfaces) {
+  if (type == kUnityGfxDeviceEventInitialize) {
+    // nothing
+  } else if (type == kUnityGfxDeviceEventShutdown) {
+    //@TODO: release resources
+  }
 }
 
 void RenderAPI_OpenGLCoreES::TextureSubImage3D(
-	void* texture_handle,
-	int32_t xoffset,
-	int32_t yoffset,
-	int32_t zoffset,
-	int32_t width,
-	int32_t height,
-	int32_t depth,
-	void* data_ptr,
-	int32_t level,
-	int32_t format,
-	int32_t type,
-	int32_t row_pitch,
-	int32_t depth_pitch
-) {
-	GLuint gltex = (GLuint)(size_t)(texture_handle);
-	glBindTexture(GL_TEXTURE_3D, gltex);
-	glTexSubImage3D(GL_TEXTURE_3D, level, xoffset, yoffset, zoffset, width, height, depth, format, type, data_ptr);
+    void* texture_handle, int32_t xoffset, int32_t yoffset, int32_t zoffset,
+    int32_t width, int32_t height, int32_t depth, void* data_ptr, int32_t level,
+    int32_t format, int32_t type, int32_t row_pitch, int32_t depth_pitch) {
+  GLuint gltex = (GLuint)(size_t)(texture_handle);
+  glBindTexture(GL_TEXTURE_3D, gltex);
+  glTexSubImage3D(GL_TEXTURE_3D, level, xoffset, yoffset, zoffset, width,
+                  height, depth, format, type, data_ptr);
 }
 
-void RenderAPI_OpenGLCoreES::TextureSubImage2D(
-	void* texture_handle,
-	int32_t xoffset,
-	int32_t yoffset,
-	int32_t width,
-	int32_t height,
-	void* data_ptr,
-	int32_t level,
-	int32_t format,
-	int32_t type,
-	int32_t row_pitch
-) {
-	GLuint gltex = (GLuint)(size_t)(texture_handle);
-	glBindTexture(GL_TEXTURE_2D, gltex);
-	glTexSubImage2D(GL_TEXTURE_2D, level, xoffset, yoffset, width, height, format, type, data_ptr);
+void RenderAPI_OpenGLCoreES::TextureSubImage2D(void* texture_handle,
+                                               int32_t xoffset, int32_t yoffset,
+                                               int32_t width, int32_t height,
+                                               void* data_ptr, int32_t level,
+                                               int32_t format, int32_t type,
+                                               int32_t row_pitch) {
+  GLuint gltex = (GLuint)(size_t)(texture_handle);
+  glBindTexture(GL_TEXTURE_2D, gltex);
+  glTexSubImage2D(GL_TEXTURE_2D, level, xoffset, yoffset, width, height, format,
+                  type, data_ptr);
 }
 
-#endif // #if SUPPORT_OPENGL_UNIFIED
+#endif  // #if SUPPORT_OPENGL_UNIFIED
--- source/TextureSubPlugin.cpp
+++ source/TextureSubPlugin.cpp
@@ -1,219 +1,178 @@
+#include <assert.h>
+#include <math.h>
+
+#include <vector>
+
 #include "PlatformBase.h"
 #include "RenderAPI.h"
 
-#include <assert.h>
-#include <math.h>
-#include <vector>
-
-
-static void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType eventType);
+static void UNITY_INTERFACE_API
+OnGraphicsDeviceEvent(UnityGfxDeviceEventType eventType);
 
 static IUnityInterfaces* s_UnityInterfaces = NULL;
 static IUnityGraphics* s_Graphics = NULL;
 
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+UnityPluginLoad(IUnityInterfaces* unityInterfaces) {
+  s_UnityInterfaces = unityInterfaces;
+  s_Graphics = s_UnityInterfaces->Get<IUnityGraphics>();
+  s_Graphics->RegisterDeviceEventCallback(OnGraphicsDeviceEvent);
 
-extern "C" void	UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces * unityInterfaces)
-{
-	s_UnityInterfaces = unityInterfaces;
-	s_Graphics = s_UnityInterfaces->Get<IUnityGraphics>();
-	s_Graphics->RegisterDeviceEventCallback(OnGraphicsDeviceEvent);
-
-	// Run OnGraphicsDeviceEvent(initialize) manually on plugin load
-	OnGraphicsDeviceEvent(kUnityGfxDeviceEventInitialize);
+  // Run OnGraphicsDeviceEvent(initialize) manually on plugin load
+  OnGraphicsDeviceEvent(kUnityGfxDeviceEventInitialize);
 }
 
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
-{
-	s_Graphics->UnregisterDeviceEventCallback(OnGraphicsDeviceEvent);
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload() {
+  s_Graphics->UnregisterDeviceEventCallback(OnGraphicsDeviceEvent);
 }
 
 // GraphicsDeviceEvent
 static RenderAPI* s_CurrentAPI = NULL;
 static UnityGfxRenderer s_DeviceType = kUnityGfxRendererNull;
 
+static void UNITY_INTERFACE_API
+OnGraphicsDeviceEvent(UnityGfxDeviceEventType eventType) {
+  // Create graphics API implementation upon initialization
+  if (eventType == kUnityGfxDeviceEventInitialize) {
+    assert(s_CurrentAPI == NULL);
+    s_DeviceType = s_Graphics->GetRenderer();
+    s_CurrentAPI = CreateRenderAPI(s_DeviceType);
+  }
 
-static void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType eventType)
-{
-	// Create graphics API implementation upon initialization
-	if (eventType == kUnityGfxDeviceEventInitialize)
-	{
-		assert(s_CurrentAPI == NULL);
-		s_DeviceType = s_Graphics->GetRenderer();
-		s_CurrentAPI = CreateRenderAPI(s_DeviceType);
-	}
+  // Let the implementation process the device related events
+  if (s_CurrentAPI) {
+    s_CurrentAPI->ProcessDeviceEvent(eventType, s_UnityInterfaces);
+  }
 
-	// Let the implementation process the device related events
-	if (s_CurrentAPI)
-	{
-		s_CurrentAPI->ProcessDeviceEvent(eventType, s_UnityInterfaces);
-	}
-
-	// Cleanup graphics API implementation upon shutdown
-	if (eventType == kUnityGfxDeviceEventShutdown)
-	{
-		delete s_CurrentAPI;
-		s_CurrentAPI = NULL;
-		s_DeviceType = kUnityGfxRendererNull;
-	}
+  // Cleanup graphics API implementation upon shutdown
+  if (eventType == kUnityGfxDeviceEventShutdown) {
+    delete s_CurrentAPI;
+    s_CurrentAPI = NULL;
+    s_DeviceType = kUnityGfxRendererNull;
+  }
 }
 
-
 struct TextureSubImage2DParams {
-	void* texture_handle;
-	int32_t xoffset;
-	int32_t yoffset;
-	int32_t width;
-	int32_t height;
-	void* data_ptr;
-	int32_t level;
-	int32_t format;
-	int32_t type;
-	int32_t row_pitch;
+  void* texture_handle;
+  int32_t xoffset;
+  int32_t yoffset;
+  int32_t width;
+  int32_t height;
+  void* data_ptr;
+  int32_t level;
+  int32_t format;
+  int32_t type;
+  int32_t row_pitch;
 };
 static TextureSubImage2DParams* g_TextureSubImage2DParams = nullptr;
 
-extern "C" void	UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetTextureSubImage2DParams(
-	void* texture_handle,
-	int32_t xoffset,
-	int32_t yoffset,
-	int32_t width,
-	int32_t height,
-	void* data_ptr,
-	int32_t level,
-	int32_t format,
-	int32_t type,
-	int32_t row_pitch
-)
-{
-	if (nullptr != g_TextureSubImage2DParams) {
-		delete g_TextureSubImage2DParams;
-	}
-	g_TextureSubImage2DParams = new TextureSubImage2DParams;
-	g_TextureSubImage2DParams->texture_handle = texture_handle;
-	g_TextureSubImage2DParams->xoffset = xoffset;
-	g_TextureSubImage2DParams->yoffset = yoffset;
-	g_TextureSubImage2DParams->width = width;
-	g_TextureSubImage2DParams->height = height;
-	g_TextureSubImage2DParams->data_ptr = data_ptr;
-	g_TextureSubImage2DParams->level = level;
-	g_TextureSubImage2DParams->format = format;
-	g_TextureSubImage2DParams->type = type;
-	g_TextureSubImage2DParams->row_pitch = row_pitch;
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+SetTextureSubImage2DParams(void* texture_handle, int32_t xoffset,
+                           int32_t yoffset, int32_t width, int32_t height,
+                           void* data_ptr, int32_t level, int32_t format,
+                           int32_t type, int32_t row_pitch) {
+  if (nullptr != g_TextureSubImage2DParams) {
+    delete g_TextureSubImage2DParams;
+  }
+  g_TextureSubImage2DParams = new TextureSubImage2DParams;
+  g_TextureSubImage2DParams->texture_handle = texture_handle;
+  g_TextureSubImage2DParams->xoffset = xoffset;
+  g_TextureSubImage2DParams->yoffset = yoffset;
+  g_TextureSubImage2DParams->width = width;
+  g_TextureSubImage2DParams->height = height;
+  g_TextureSubImage2DParams->data_ptr = data_ptr;
+  g_TextureSubImage2DParams->level = level;
+  g_TextureSubImage2DParams->format = format;
+  g_TextureSubImage2DParams->type = type;
+  g_TextureSubImage2DParams->row_pitch = row_pitch;
 }
 
 struct TextureSubImage3DParams {
-	void* texture_handle;
-	int32_t xoffset;
-	int32_t yoffset;
-	int32_t zoffset;
-	int32_t width;
-	int32_t height;
-	int32_t depth;
-	void* data_ptr;
-	int32_t level;
-	int32_t format;
-	int32_t type;
-	int32_t row_pitch;
-	int32_t depth_pitch;
+  void* texture_handle;
+  int32_t xoffset;
+  int32_t yoffset;
+  int32_t zoffset;
+  int32_t width;
+  int32_t height;
+  int32_t depth;
+  void* data_ptr;
+  int32_t level;
+  int32_t format;
+  int32_t type;
+  int32_t row_pitch;
+  int32_t depth_pitch;
 };
 static TextureSubImage3DParams* g_TextureSubImage3DParams = nullptr;
 
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetTextureSubImage3DParams(
-	void* texture_handle,
-	int32_t xoffset,
-	int32_t yoffset,
-	int32_t zoffset,
-	int32_t width,
-	int32_t height,
-	int32_t depth,
-	void* data_ptr,
-	int32_t level,
-	int32_t format,
-	int32_t type,
-	int32_t row_pitch,
-	int32_t depth_pitch
-) {
-	if (nullptr != g_TextureSubImage3DParams) {
-		delete g_TextureSubImage3DParams;
-	}
-	g_TextureSubImage3DParams = new TextureSubImage3DParams;
-	g_TextureSubImage3DParams->texture_handle = texture_handle;
-	g_TextureSubImage3DParams->xoffset = xoffset;
-	g_TextureSubImage3DParams->yoffset = yoffset;
-	g_TextureSubImage3DParams->zoffset = zoffset;
-	g_TextureSubImage3DParams->width = width;
-	g_TextureSubImage3DParams->height = height;
-	g_TextureSubImage3DParams->depth = depth;
-	g_TextureSubImage3DParams->data_ptr = data_ptr;
-	g_TextureSubImage3DParams->level = level;
-	g_TextureSubImage3DParams->format = format;
-	g_TextureSubImage3DParams->type = type;
-	g_TextureSubImage3DParams->row_pitch = row_pitch;
-	g_TextureSubImage3DParams->depth_pitch = depth_pitch;
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+SetTextureSubImage3DParams(void* texture_handle, int32_t xoffset,
+                           int32_t yoffset, int32_t zoffset, int32_t width,
+                           int32_t height, int32_t depth, void* data_ptr,
+                           int32_t level, int32_t format, int32_t type,
+                           int32_t row_pitch, int32_t depth_pitch) {
+  if (nullptr != g_TextureSubImage3DParams) {
+    delete g_TextureSubImage3DParams;
+  }
+  g_TextureSubImage3DParams = new TextureSubImage3DParams;
+  g_TextureSubImage3DParams->texture_handle = texture_handle;
+  g_TextureSubImage3DParams->xoffset = xoffset;
+  g_TextureSubImage3DParams->yoffset = yoffset;
+  g_TextureSubImage3DParams->zoffset = zoffset;
+  g_TextureSubImage3DParams->width = width;
+  g_TextureSubImage3DParams->height = height;
+  g_TextureSubImage3DParams->depth = depth;
+  g_TextureSubImage3DParams->data_ptr = data_ptr;
+  g_TextureSubImage3DParams->level = level;
+  g_TextureSubImage3DParams->format = format;
+  g_TextureSubImage3DParams->type = type;
+  g_TextureSubImage3DParams->row_pitch = row_pitch;
+  g_TextureSubImage3DParams->depth_pitch = depth_pitch;
 }
 
-enum Event
-{
-	TextureSubImage2D = 0,
-	TextureSubImage3D = 1,
+enum Event {
+  TextureSubImage2D = 0,
+  TextureSubImage3D = 1,
 };
 
+static void UNITY_INTERFACE_API OnRenderEvent(int eventID) {
+  // Unknown / unsupported graphics device type? Do nothing
+  if (s_CurrentAPI == NULL) return;
 
-static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
-{
-	// Unknown / unsupported graphics device type? Do nothing
-	if (s_CurrentAPI == NULL)
-		return;
-
-	switch ((Event)eventID)
-	{
-	case Event::TextureSubImage2D: {
-		if (nullptr == g_TextureSubImage2DParams) return;
-		s_CurrentAPI->TextureSubImage2D(
-			g_TextureSubImage2DParams->texture_handle,
-			g_TextureSubImage2DParams->xoffset,
-			g_TextureSubImage2DParams->yoffset,
-			g_TextureSubImage2DParams->width,
-			g_TextureSubImage2DParams->height,
-			g_TextureSubImage2DParams->data_ptr,
-			g_TextureSubImage2DParams->level,
-			g_TextureSubImage2DParams->format,
-			g_TextureSubImage2DParams->type,
-			g_TextureSubImage2DParams->row_pitch
-		);
-		delete g_TextureSubImage2DParams;
-		break;
-	}
-	case Event::TextureSubImage3D: {
-		if (nullptr == g_TextureSubImage3DParams) return;
-		s_CurrentAPI->TextureSubImage3D(
-			g_TextureSubImage3DParams->texture_handle,
-			g_TextureSubImage3DParams->xoffset,
-			g_TextureSubImage3DParams->yoffset,
-			g_TextureSubImage3DParams->zoffset,
-			g_TextureSubImage3DParams->width,
-			g_TextureSubImage3DParams->height,
-			g_TextureSubImage3DParams->depth,
-			g_TextureSubImage3DParams->data_ptr,
-			g_TextureSubImage3DParams->level,
-			g_TextureSubImage3DParams->format,
-			g_TextureSubImage3DParams->type,
-			g_TextureSubImage3DParams->row_pitch,
-			g_TextureSubImage3DParams->depth_pitch
-		);
-		delete g_TextureSubImage3DParams;
-		break;
-	}
-	default:
-		break;
-	}
-
-
+  switch ((Event)eventID) {
+    case Event::TextureSubImage2D: {
+      if (nullptr == g_TextureSubImage2DParams) return;
+      s_CurrentAPI->TextureSubImage2D(
+          g_TextureSubImage2DParams->texture_handle,
+          g_TextureSubImage2DParams->xoffset,
+          g_TextureSubImage2DParams->yoffset, g_TextureSubImage2DParams->width,
+          g_TextureSubImage2DParams->height,
+          g_TextureSubImage2DParams->data_ptr, g_TextureSubImage2DParams->level,
+          g_TextureSubImage2DParams->format, g_TextureSubImage2DParams->type,
+          g_TextureSubImage2DParams->row_pitch);
+      delete g_TextureSubImage2DParams;
+      break;
+    }
+    case Event::TextureSubImage3D: {
+      if (nullptr == g_TextureSubImage3DParams) return;
+      s_CurrentAPI->TextureSubImage3D(
+          g_TextureSubImage3DParams->texture_handle,
+          g_TextureSubImage3DParams->xoffset,
+          g_TextureSubImage3DParams->yoffset,
+          g_TextureSubImage3DParams->zoffset, g_TextureSubImage3DParams->width,
+          g_TextureSubImage3DParams->height, g_TextureSubImage3DParams->depth,
+          g_TextureSubImage3DParams->data_ptr, g_TextureSubImage3DParams->level,
+          g_TextureSubImage3DParams->format, g_TextureSubImage3DParams->type,
+          g_TextureSubImage3DParams->row_pitch,
+          g_TextureSubImage3DParams->depth_pitch);
+      delete g_TextureSubImage3DParams;
+      break;
+    }
+    default:
+      break;
+  }
 }
 
-extern "C" UnityRenderingEvent UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API GetRenderEventFunc()
-{
-	return OnRenderEvent;
+extern "C" UnityRenderingEvent UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+GetRenderEventFunc() {
+  return OnRenderEvent;
 }
-
-
