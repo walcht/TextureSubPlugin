c083967d93ab698ca48b68f0cba755061335dd00
Walid Chtioui <walid.chtioui@ensi-uma.tn>
2024-09-16 14:30:09 +0200

    initial commit;

---
+++ source/RenderAPI.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "Unity/IUnityGraphics.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+struct IUnityInterfaces;
+
+class RenderAPI
+{
+public:
+	virtual ~RenderAPI() { }
+
+	/// <summary>
+	///		Loads provided data (i.e., sub-region of texture 2D) into a provided texture2D's GPU memory.
+	/// </summary>
+	/// 
+	/// <remarks>
+	///		For OpenGL this is simply a call to glTextureSubImage2D.
+	/// </remarks>
+	/// 
+	/// <param name="texture_handle">
+	///		
+	/// </param>
+	/// 
+	/// <param name="xoffset"></param>
+	/// <param name="yoffset"></param>
+	/// <param name="width"></param>
+	/// <param name="height"></param>
+	/// 
+	/// <param name="data_ptr"></param>
+	/// 
+	/// <param name="level">
+	///		Will be provided to glTextureSubImage2D's level parameter.
+	///		Only used in case graphics API is OpenGL.
+	/// </param>
+	/// 
+	/// <param name="format">
+	///		Will be provided to glTextureSubImage2D's format parameter.
+	///		Only used in case graphics API is OpenGL.
+	/// </param>
+	/// 
+	/// <param name="type">
+	///		Will be provided to glTextureSubImage2D's type parameter.
+	///		Only used in case graphics API is OpenGL.
+	///	</param>
+	/// 
+	/// <param name="row_pitch">
+	///		The size in bytes of one row of the source data. Will be provided, as is, to UpdateSubresource's
+	///		SrcRowPitch parameter. Only used in case graphics API is Direct3D11.
+	/// </param>
+	virtual void TextureSubImage2D(
+		void* texture_handle,
+		int32_t xoffset,
+		int32_t yoffset,
+		int32_t width,
+		int32_t height,
+		void* data_ptr,
+		int32_t level = 0,
+		int32_t format = -1,
+		int32_t type = -1,
+		int32_t row_pitch = -1
+	) = 0;
+
+	virtual void TextureSubImage3D(
+		void* texture_handle,
+		int32_t xoffset,
+		int32_t yoffset,
+		int32_t zoffset,
+		int32_t width,
+		int32_t height,
+		int32_t depth,
+		void* data_ptr,
+		int32_t level = 0,
+		int32_t format = -1,
+		int32_t type = -1,
+		int32_t row_pitch = -1,
+		int32_t depth_pitch = -1
+	) = 0;
+
+	/// <summary>
+	///		Callback to process general events like initialization, shutdown, device loss/reset etc.		
+	/// </summary>
+	/// 
+	/// <param name="type">
+	/// </param>
+	/// 
+	/// <param name="interfaces">
+	/// </param>
+	virtual void ProcessDeviceEvent(UnityGfxDeviceEventType type, IUnityInterfaces* interfaces) = 0;
+};
+
+
+// Create a graphics API implementation instance for the given API type.
+RenderAPI* CreateRenderAPI(UnityGfxRenderer apiType);
+
---
+++ source/RenderAPI_D3D11.cpp
@@ -0,0 +1,145 @@
+#include "RenderAPI.h"
+#include "PlatformBase.h"
+
+// Direct3D 11 implementation of RenderAPI.
+
+#if SUPPORT_D3D11
+
+#include <assert.h>
+#include <d3d11.h>
+#include "Unity/IUnityGraphicsD3D11.h"
+
+
+class RenderAPI_D3D11 : public RenderAPI
+{
+public:
+	RenderAPI_D3D11();
+	virtual ~RenderAPI_D3D11() { }
+
+	virtual void ProcessDeviceEvent(UnityGfxDeviceEventType type, IUnityInterfaces* interfaces);
+	virtual void TextureSubImage2D(
+		void* texture_handle,
+		int32_t xoffset,
+		int32_t yoffset,
+		int32_t width,
+		int32_t height,
+		void* data_ptr,
+		int32_t level = 0,
+		int32_t format = -1,
+		int32_t type = -1,
+		int32_t row_pitch = -1
+	);
+	virtual void TextureSubImage3D(
+		void* texture_handle,
+		int32_t xoffset,
+		int32_t yoffset,
+		int32_t zoffset,
+		int32_t width,
+		int32_t height,
+		int32_t depth,
+		void* data_ptr,
+		int32_t level = 0,
+		int32_t format = -1,
+		int32_t type = -1,
+		int32_t row_pitch = -1,
+		int32_t depth_pitch = -1
+	);
+
+private:
+	ID3D11Device* m_Device;
+};
+
+
+RenderAPI* CreateRenderAPI_D3D11()
+{
+	return new RenderAPI_D3D11();
+}
+
+
+RenderAPI_D3D11::RenderAPI_D3D11()
+	: m_Device(NULL)
+{
+}
+
+void RenderAPI_D3D11::ProcessDeviceEvent(UnityGfxDeviceEventType type, IUnityInterfaces* interfaces)
+{
+	switch (type)
+	{
+	case kUnityGfxDeviceEventInitialize:
+	{
+		IUnityGraphicsD3D11* d3d = interfaces->Get<IUnityGraphicsD3D11>();
+		m_Device = d3d->GetDevice();
+		break;
+	}
+	case kUnityGfxDeviceEventShutdown:
+		break;
+	}
+}
+
+// const int rowPitch = textureWidth * 4;
+void RenderAPI_D3D11::TextureSubImage2D(
+	void* texture_handle,
+	int32_t xoffset,
+	int32_t yoffset,
+	int32_t width,
+	int32_t height,
+	void* data_ptr,
+	int32_t level,
+	int32_t format,
+	int32_t type,
+	int32_t row_pitch
+) {
+	ID3D11Texture2D* d3dtex = (ID3D11Texture2D*)texture_handle;
+	assert(d3dtex);
+	ID3D11DeviceContext* ctx = NULL;
+	m_Device->GetImmediateContext(&ctx);
+
+	// Update texture data, and free the memory buffer
+	D3D11_BOX box;
+	// make sure to set front and back such that front < back
+	// even if they are not needed so that this works
+	box.left = xoffset;
+	box.top = yoffset;
+	box.front = 0;
+	box.right = xoffset + width;
+	box.bottom = yoffset + height;
+	box.back = 1;
+
+	ctx->UpdateSubresource(d3dtex, 0, &box, data_ptr, row_pitch, 0);
+	ctx->Release();
+}
+
+void RenderAPI_D3D11::TextureSubImage3D(
+	void* texture_handle,
+	int32_t xoffset,
+	int32_t yoffset,
+	int32_t zoffset,
+	int32_t width,
+	int32_t height,
+	int32_t depth,
+	void* data_ptr,
+	int32_t level,
+	int32_t format,
+	int32_t type,
+	int32_t row_pitch,
+	int32_t depth_pitch
+) {
+	ID3D11Texture2D* d3dtex = (ID3D11Texture2D*)texture_handle;
+	assert(d3dtex);
+	ID3D11DeviceContext* ctx = NULL;
+	m_Device->GetImmediateContext(&ctx);
+
+	// Update texture data, and free the memory buffer
+	D3D11_BOX box;
+	box.left = xoffset;
+	box.top = yoffset;
+	box.front = zoffset;
+	box.right = xoffset + width;
+	box.bottom = yoffset + height;
+	box.back = zoffset + depth;
+
+	ctx->UpdateSubresource(d3dtex, 0, &box, data_ptr, row_pitch, depth_pitch);
+	ctx->Release();
+}
+
+#endif // #if SUPPORT_D3D11
